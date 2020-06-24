b7d8f18819359b7754f4f73377f8923bb7cbe0b1
nithinnan <39975220+nithinnan@users.noreply.github.com>
2020-06-24 17:00:13 +0800

    Update webapp-db.tf

--- Terraform/webapp-db.tf
+++ Terraform/webapp-db.tf
@@ -48,7 +48,7 @@ resource "azurerm_app_service" "app-service" {
 }
 
 resource "azurerm_sql_server" "sqldb" {
-  name                         = "terraform-sqlserver"
+  name                         = "terraform-sqlserver-sia"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
